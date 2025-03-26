4790b0f091dd1da7d50b72b1374f8fa1e686fcb5
Jan1Verse1 <oluwaseyiadedotun@gmail.com>
2025-03-26 04:23:30 +0100

    cleaned up tree

--- dinero-tech/src/App.jsx
+++ dinero-tech/src/App.jsx
@@ -1,37 +1,8 @@
-// import React from "react";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-// import Home from "./Pages/Home";
-
-// // import FAQs from "../src/Pages/FAQs";
-// // import ContactUs from "../src/Pages/ContactUs";
-// // import AboutUs from "../src/Pages/AboutUs";
-// // import Communal from "../src/Pages/Communal";
-// // import Ajoo from "../src/Pages/Ajoo";
-// // import Calculator from "./Pages/Calculator";
-// // import Score from "../src/Pages/Score";
-
-// const router = createBrowserRouter([
-//   { path: "/", element: <Home /> },
-//   // { path: "/contactus", element: <ContactUs /> },
-//   // { path: "/about", element: <AboutUs /> },
-//   // { path: "/faqs", element: <FAQs /> },
-//   // { path: "/communal", element: <Communal /> },
-//   // { path: "/ajoo", element: <Ajoo /> },
-//   // { path: "/calculator", element: <Calculator /> },
-//   // { path: "/score", element: <Score /> },
-// ]);
-
-// const App = () => {
-//   return <RouterProvider router={router} />;
-// };
-
 import React, { Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const Home = React.lazy(() => import("./Pages/Home"));
-const WhoWeAre = React.lazy(() => import("./Pages/Who"));
-const WhatWeDo = React.lazy(() => import("./Pages/What"));
+
 const Contact = React.lazy(() => import("./Pages/Contact"));
 
 // const ContactUs = React.lazy(() => import("./Pages/ContactUs"));
@@ -42,22 +13,6 @@ const router = createBrowserRouter([
     element: (
       <Suspense fallback={<div>Loading...</div>}>
         <Home />
-      </Suspense>
-    ),
-  },
-  {
-    path: "/WhoWeAre",
-    element: (
-      <Suspense fallback={<div>Loading...</div>}>
-        <WhoWeAre />
-      </Suspense>
-    ),
-  },
-  {
-    path: "/WhatWeDo",
-    element: (
-      <Suspense fallback={<div>Loading...</div>}>
-        <WhatWeDo />
       </Suspense>
     ),
   },
